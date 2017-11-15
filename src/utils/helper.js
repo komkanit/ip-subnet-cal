83c2f421814b1dc3e6bbe9dec64df96bbc8b3040
komcal <kom.sujautra@gmail.com>
2017-11-15 18:07:20 +0700

    update function

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,1 +1,49 @@
+export const convertToSubnet = (a) => {
+  const subnet = [0, 0, 0, 0].map(() => {
+    const sub = '00000000'.split('').map((zero) => {
+      a -= 1;
+      return a >= 0 ? '1' : '0';
+    })
+    return parseInt(sub.join(''), 2);
+  });
+  return subnet.join('.');
+}
+
+export const networkClassSplit = (type) => {
+  const typeLength = {
+    any: 1,
+    a: 8,
+    b: 16,
+    c: 24,
+  };
+  const subnet = new Array(32 - typeLength[type] + 1).fill(0);
+  return subnet.map((s, index) =>
+    `${convertToSubnet(index + typeLength[type])} / ${index + typeLength[type]}`
+  ).reverse();
+}
+const decimalIpToBinary = ip =>
+  ip.split('.')
+  .map(ipChar => '0'.repeat(8 - (+ipChar).toString(2).length) + (+ipChar).toString(2))
+  .join('')
+
+const binaryIpToDecimal = binary =>
+  [0, 0, 0, 0].map((zero, index) => parseInt(binary.substr(index * 8, 8), 2))
+  .join('.');
+
+export const ipToNetAddress = (ip, subnet) => {
+  const binaryIp = decimalIpToBinary(ip)
+  .split('')
+  .map(binaryChar => subnet-- > 0 ? binaryChar : '0')
+  .join('');
+  return binaryIpToDecimal(binaryIp);
+}
+
+export const ipToBroadcast = (ip, subnet) => {
+  const binaryIp = decimalIpToBinary(ip)
+  .split('')
+  .map(binaryChar => subnet-- > 0 ? binaryChar : '1')
+  .join('');
+  return binaryIpToDecimal(binaryIp);
+}
+
 export const plus = (x, y) => x + y;
--- src/utils/helper.test.js
+++ src/utils/helper.test.js
@@ -1,9 +1,57 @@
 import { expect } from 'chai';
-import { plus } from './helper';
+import { plus,
+  convertToSubnet,
+  networkClassSplit,
+  ipToNetAddress,
+  ipToBroadcast,
+} from './helper';
 
 describe('test plus', () => {
   it('should plus number', () => {
     expect(plus(1, 2)).to.equal(3);
     expect(plus(0, 2)).to.equal(2);
   })
+})
+
+describe('test convertToSubnet', () => {
+  it('should convert to subnet', () => {
+    expect(convertToSubnet(1)).to.equal('128.0.0.0');
+    expect(convertToSubnet(2)).to.equal('192.0.0.0');
+    expect(convertToSubnet(24)).to.equal('255.255.255.0');
+    expect(convertToSubnet(15)).to.equal('255.254.0.0');
+  });
+})
+
+describe('test networkClassSplit', () => {
+  it('should show arry of subnet', () => {
+    const expectedValue = [
+      `${convertToSubnet(32)} / 32`,
+      `${convertToSubnet(31)} / 31`,
+      `${convertToSubnet(30)} / 30`,
+      `${convertToSubnet(29)} / 29`,
+      `${convertToSubnet(28)} / 28`,
+      `${convertToSubnet(27)} / 27`,
+      `${convertToSubnet(26)} / 26`,
+      `${convertToSubnet(25)} / 25`,
+      `${convertToSubnet(24)} / 24`,
+    ]
+    const value = networkClassSplit('c');
+    value.map((subnet, index) => {
+      expect(subnet).to.equal(expectedValue[index]);
+    });
+  });
+})
+
+describe('ipToNetAddress test', () => {
+  it('should show network address', () => {
+    expect(ipToNetAddress('159.108.12.111', 28)).to.equal('159.108.12.96');
+    expect(ipToNetAddress('159.228.12.111', 13)).to.equal('159.224.0.0');
+    expect(ipToNetAddress('158.108.12.34', 21)).to.equal('158.108.8.0');
+  });
+})
+
+describe('ipToBroadcast test', () => {
+  it('should show broad case address', () => {
+    expect(ipToBroadcast('158.108.12.34', 21)).to.equal('158.108.15.255');
+  });
 })
